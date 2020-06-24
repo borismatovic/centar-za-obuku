738fc16e10a752d4d250d588b366bf04e298dd50
boris matovic <boris.matovic@gmail.com>
2020-06-24 16:31:18 +0200

    +lightbox, news page changes

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-let element = document.querySelector('.to-top');
+// scroll page to the top
+let element = document.querySelector('.to-top')
 window.addEventListener("scroll", function () {
     if (pageYOffset > 100) { element.classList.remove('hidden') }
-    else element.classList.add('hidden');
+    else element.classList.add('hidden')
 });
+
+// basic lightbox for the NEWS page
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('.gallery-image')
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+})
+
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
+})
